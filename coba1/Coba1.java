50beae7e6941d7f39d212aed9829f2644cbc11e3
dikifahrul <142971613+dikifahrul@users.noreply.github.com>
2024-08-06 15:34:28 +0700

    pemrograman visual modul 1,2 dan 3

---
+++ coba1/Coba1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package coba.coba1;
+
+
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Coba1 {
+
+    public static void main(String[] args) {
+       modul3 modul=new modul3();
+       modul.setVisible(true);
+    }
+}
